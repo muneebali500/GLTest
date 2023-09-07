0cbd86372db0177fed53dae2271dac7d443a40eb
Muneeb Ali <muneebali500@gmail.com>
2023-09-07 18:51:02 +0500

    made page responsive

--- js/index.js
+++ js/index.js
@@ -21,6 +21,36 @@ const sections = [
   "footer",
 ].map((id) => document.querySelector(`#${id}`));
 
+/////////// function displays side navbar on small screens /////////////
+const hamburger = document.getElementById("hamburger");
+const crossMark = document.getElementById("cross-mark");
+const sidebar = document.getElementById("header-nav");
+
+hamburger.addEventListener("click", () => {
+  sidebar.style.transform = "translateX(0)";
+  hamburger.style.display = "none";
+  crossMark.style.display = "block";
+});
+
+crossMark.addEventListener("click", () => {
+  sidebar.style.transform = "translateX(-100%)";
+  hamburger.style.display = "block";
+  crossMark.style.display = "none";
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    sidebar.style.display = "flex";
+    sidebar.style.transform = "translateX(0)";
+    hamburger.style.display = "none";
+    crossMark.style.display = "none";
+  } else {
+    hamburger.style.display = "block";
+    sidebar.style.transform = "translateX(-100%)";
+    crossMark.style.display = "none";
+  }
+});
+
 /////////// EVENT LISTENER SHOWS HERO SECTION HEADERS ON INITIAL LOAD /////////////
 window.addEventListener("load", () => {
   heroHeaderEl.classList.remove("hidden");
@@ -28,17 +58,19 @@ window.addEventListener("load", () => {
 
 /////////// FUNCTION TO HANDLE SCROLL ELEMENTS /////////////
 window.addEventListener("scroll", () => {
-  headerEl.classList.toggle("fixed", window.pageYOffset > 120);
+  if (window.innerWidth > 770) {
+    headerEl.classList.toggle("fixed", window.pageYOffset > 120);
+
+    sections.forEach((section, index) => setActiveNavLink(section, index));
+  }
 
   showHiddenElement(teamHeaderEl);
   showHiddenElement(productsHeaderEl);
-
-  sections.forEach((section, index) => setActiveNavLink(section, index));
 });
 
 // Function to set the "active" class on the appropriate headerNavLink
 function setActiveNavLink(section, index) {
-  if (isElementXPercentInViewport(section, 20)) {
+  if (isElementXPercentInViewport(section, 35)) {
     headerNavLinks.forEach((link) => link.classList.remove("active"));
     headerNavLinks[index].classList.add("active");
   }
